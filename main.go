a9e2fc2bca9a293f8eafbfb6ed85c5b7b6d251b1
pawelbalcerek <pawbal96@gmail.com>
2026-05-17 18:29:29 +0200

    serve files from root path

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	root, port := ".", 8080
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(root)))
 	s := &http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf(":%d", port),
 		Handler: mux,
 	}
-	s.ListenAndServe()
+	log.Println(fmt.Sprintf("Serving files from path \"%s\" on localhost:%d", root, port))
+	log.Fatal(s.ListenAndServe())
 }
